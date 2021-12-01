d5d17109c1727a7a8f17b88e1947c81ed2a745a3
gusehd66 <rlagusehd45@naver.com>
2021-12-01 14:54:12 +0900

    :memo: Image data update

--- src/data/images.js
+++ src/data/images.js
@@ -5,17 +5,27 @@ export const itemData = [
       {
         img: "https://user-images.githubusercontent.com/76733178/143682277-de55590c-61e6-4f66-b952-db8dc38555ec.png",
         title: "맨투맨 OATMEAL",
-        desc: "유니폼브릿지",
+        desc: "유니폼 브릿지",
       },
       {
         img: "https://user-images.githubusercontent.com/76733178/143682281-253d7d65-b6e1-4151-9720-99932704dd0d.png",
-        title: "체크셔츠",
+        title: "체크셔츠 NAVY",
         desc: "다이아몬드 레이라",
       },
       {
         img: "https://user-images.githubusercontent.com/76733178/143682282-0a56183f-e9b6-420e-9d72-09b260222ca7.png",
-        title: "오버핏 니트",
+        title: "오버핏 니트 GRAY",
         desc: "미니멀 프로젝트",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178409-9702aa3d-8acd-4b63-8aa9-c8a7d511839b.png",
+        title: "단추 후드티 GRAY",
+        desc: "PHOS333",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178406-af5b6486-4af4-4405-8c57-7e62f1939040.png",
+        title: "맨투맨 NAVY",
+        desc: "유니폼 브릿지",
       },
     ],
   },
@@ -24,13 +34,33 @@ export const itemData = [
     items: [
       {
         img: "https://user-images.githubusercontent.com/76733178/143682272-9e584cd1-bba2-4550-aee8-9619f42a8dd4.png",
-        title: "슬랙스 GREY",
+        title: "슬랙스 GRAY",
         desc: "보세",
       },
       {
         img: "https://user-images.githubusercontent.com/76733178/143682275-86f320a3-4d24-4081-bc0a-c7376f7d2892.png",
         title: "세미와이드 청바지",
         desc: "스파오",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178405-170bd3bd-03be-407e-a22b-feed049469be.png",
+        title: "흑청바지",
+        desc: "에잇세컨즈",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178402-4e0b0682-01fd-4b91-b246-d8ef54f5059f.png",
+        title: "크림진",
+        desc: "에잇세컨즈",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178399-98a76875-8a84-4960-bec0-5f3e754a0dee.png",
+        title: "투턱 슬랙스 BEIGE",
+        desc: "시그니처",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178397-ee2193b0-9417-41a2-b6cd-006080951419.png",
+        title: "트레이닝 바지 BLACK",
+        desc: "보세",
       },
     ],
   },
@@ -47,6 +77,26 @@ export const itemData = [
         title: "뉴발란스 237",
         desc: "뉴발란스",
       },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178393-7794b318-3f67-4e71-8208-e6130f1a843a.png",
+        title: "에라 MUSTARD",
+        desc: "VANS",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178394-0a45bbc1-d896-4b4a-a1f9-5fd3c0f8ae0f.png",
+        title: "올드스쿨 스웨이드 BLUE",
+        desc: "VANS",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178401-6a2af919-4636-48a3-a3f0-546d2a94f51c.png",
+        title: "YUNG-96 BLACK",
+        desc: "아디다스",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178411-80d02301-d981-4e93-b1f7-1cb6bc91059b.png",
+        title: "테일윈드 데님",
+        desc: "나이키",
+      },
     ],
   },
   {
@@ -61,6 +111,16 @@ export const itemData = [
         img: "https://user-images.githubusercontent.com/76733178/143682278-6755d6c7-7727-4128-9e17-bdce592b36f6.png",
         title: "코듀로이 MA-1",
         desc: "커버낫",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178392-d0d4da48-b500-4af4-86c2-7eeb29b23355.png",
+        title: "롱패딩 BLACK",
+        desc: "마인드브릿지",
+      },
+      {
+        img: "https://user-images.githubusercontent.com/76733178/144178386-f2779662-3a4f-4fda-b1ab-e378c9c9950c.png",
+        title: "리버시블 패딩",
+        desc: "네파",
       },
     ],
   },
