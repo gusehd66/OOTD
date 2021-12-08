726d44e93097c4eacac6392db8a64ea575310fc5
gusehd66 <rlagusehd45@naver.com>
2021-12-08 14:02:17 +0900

    :art: RandomResult component 에서 비어있는 context 확인 상태 관리

--- src/components/RandomResult.jsx
+++ src/components/RandomResult.jsx
@@ -1,19 +1,15 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useCallback, useState } from "react";
 import { SelectContext } from "../context/context";
 import ResultComponent from "./ResultComponent";
 import { itemData } from "../data/images";
 import { Button } from "@mui/material";
 
 const RandomResult = () => {
-  const { select, setSelect } = useContext(SelectContext);
+  const { setSelect } = useContext(SelectContext);
+  const [checkEmpty, setCheckEmpty] = useState(false);
 
   const onClick = () => {
-    setSelect({
-      상의: "",
-      하의: "",
-      신발: "",
-      아우터: "",
-    });
+    cleanContext();
     itemData.map((key) => {
       const randomItem = Math.floor(Math.random() * key.items.length);
       return setSelect((prev) => {
@@ -21,27 +17,33 @@ const RandomResult = () => {
         return prev;
       });
     });
+    setCheckEmpty(true);
   };
 
-  useEffect(() => {
-    return () => {
-      setSelect({
-        상의: "",
-        하의: "",
-        신발: "",
-        아우터: "",
-      });
-    };
+  const cleanContext = useCallback(() => {
+    setSelect({
+      상의: "",
+      하의: "",
+      신발: "",
+      아우터: "",
+    });
   }, [setSelect]);
+
+  useEffect(() => {
+    return () => cleanContext();
+  }, [cleanContext]);
 
   return (
     <>
-      <ResultComponent />
+      {checkEmpty && <ResultComponent />}
       <Button
         variant="contained"
         color="primary"
         onClick={onClick}
-        sx={{ marginLeft: "50%", transform: "translateX(-50%)" }}
+        sx={{
+          marginLeft: "50%",
+          transform: "translateX(-50%)",
+        }}
       >
         RANDOM
       </Button>
--- src/components/ResultComponent.jsx
+++ src/components/ResultComponent.jsx
@@ -1,20 +1,14 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { SelectContext } from "../context/context";
 
 //css 수정 필요
 const ResultComponent = () => {
   const { select } = useContext(SelectContext);
   const keys = Object.keys(select);
-  const transX = [20, 20, 20, 5];
+  const transX = [25, 25, 20, 5];
   const transY = [10, 30, 60, 6];
   const zIndex = [4, 2, 3, 1];
 
-  useEffect(() => {
-    keys.map((key) => {
-      console.log(select[key] !== "" ? "check" : "");
-      return select[key] !== "" ? "check" : "";
-    });
-  });
   return (
     <div
       style={{
