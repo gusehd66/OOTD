bd0e60fbf86043e0ed738119159331485a1e15c5
gusehd66 <rlagusehd45@naver.com>
2021-12-30 21:09:49 +0900

    :boom::boom: express mongodb 연결

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_actions";
+
+const LoginPage = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const onEmailChange = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordChange = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const body = {
+      email,
+      password,
+    };
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        // history("/");
+        props.history.push("/");
+      } else {
+        alert("Error");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={email} onChange={onEmailChange} />
+
+        <label>Password</label>
+        <input type="password" value={password} onChange={onPasswordChange} />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
